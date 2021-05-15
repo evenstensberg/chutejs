e600b468916661b36ba9b57d1dc141ea64bcf899
Even Stensberg <evenstensberg@gmail.com>
2021-05-15 18:13:12 +0200

    chore: wip on interface

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,5 +1,39 @@
 'use strict';
 
+
+class ChuteJS {
+    constructor(options) {
+
+    }
+
+
+    configureReporters() {
+
+    }
+
+    beforeTests() {
+
+    }
+
+    afterTestCompleted() {
+
+    }
+
+    onError() {
+
+    }
+
+
+    beforeLint() {
+
+    }
+
+    afterLint() {
+    }
+    afterCompleted() {
+    }
+}
+
 export default function core() {
     // TODO
 }
